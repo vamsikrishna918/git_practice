90526fd9073543fac1d23fac2b661e3cb0256abd
vamsikrishna918 <vamsiannadanam918@gmail.com>
2023-04-10 01:08:45 +0530

    initial upload

---
+++ DynamicProject/src/main/java/com/Demo/Viewpage.java
@@ -0,0 +1,12 @@
+package com.Demo;
+
+public class Viewpage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to dynamic project");
+
+	}
+
+}
